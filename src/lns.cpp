39b71dd9f13c0a440acb8b620f1e0f2601015a64
Michał Gibas <259563@student.pwr.edu.pl>
2022-06-07 22:10:40 +0200

    Fix issuses with precision of LDIV

--- src/lns.cpp
+++ src/lns.cpp
@@ -6,12 +6,16 @@ float LDIV(FP16 a, FP16 b) {
     float result;
     uint16_t k1 = approximateLog2(a.mantissa);
     uint16_t k2 = approximateLog2(b.mantissa);
+    
+    float m;
 
-    result = pow(2.0f, (a.exponent + k1/1024.0f) - (b.exponent + k2/1024.0f));
+    if(k2 > k1) {
+        m = (1.0f / (approximateAntilog2(k2 - k1) / 1024.0f)); 
+    } else {
+        m = approximateAntilog2(k1 - k2) / 1024.0f;
+    }
 
-    //TODO: figure out why the antilog method does not work
-
-    //result = ((approximateAntilog2((k1 - k2) + 1)/1024.0f)+1.0f)*pow(2.0f, a.exponent-b.exponent);
+    result = powf(2.0f, (float)(a.exponent - b.exponent))*m;
 
     return result;
 }
@@ -20,7 +24,10 @@ float LSR(FP16 a) {
     float result;
     uint16_t k1 = approximateLog2(a.mantissa);
 
-    result = powf(2.0f, 0.5f*(a.exponent + k1/1024.0f) - 7.5f);
+    float m = approximateAntilog2(k1) / 1024.0f;
+    m = powf(m, 0.5f);
+
+    result = powf(2.0f, 0.5f*(a.exponent) - 7.5f)*m;
 
     return result;
 }
