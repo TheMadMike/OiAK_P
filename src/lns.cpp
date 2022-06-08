5f50cb3f3145e3038d69f65d56d7b061bc44af42
Michał Gibas <259563@student.pwr.edu.pl>
2022-06-08 23:39:35 +0200

    Add LMUL

--- src/lns.cpp
+++ src/lns.cpp
@@ -20,6 +20,19 @@ float LDIV(FP16 a, FP16 b) {
     return result;
 }
 
+float LMUL(FP16 a, FP16 b) {
+    float result;
+    uint16_t k1 = approximateLog2(a.mantissa);
+    uint16_t k2 = approximateLog2(b.mantissa);
+
+    float m;
+    m = approximateAntilog2((k1 + k2) & 1023) / 1024.0f;
+
+    result = powf(2.0f, (float)(a.exponent + b.exponent - 30))*m - 0.5f;
+
+    return result;
+}
+
 float LSR(FP16 a) {
     float result;
     uint16_t k1 = approximateLog2(a.mantissa);
--- src/lns.hpp
+++ src/lns.hpp
@@ -4,13 +4,21 @@
 #include "fp16.hpp"
 
 /**
- * @brief Divides the two half precision floats using LNS
+ * @brief Divides the two half precision floats
  * 
- * @param a 
- * @param b 
+ * @param a dividend
+ * @param b divisor
  * @return float a / b
  */
 float LDIV(FP16 a, FP16 b);
+/**
+ * @brief Multiplies the two half precision floats 
+ * 
+ * @param a first operand
+ * @param b second operand
+ * @return float a * b
+ */
+float LMUL(FP16 a, FP16 b);
 /**
  * @brief Calculates the square root of half precision float
  * 
