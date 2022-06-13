42fa946a98d253a1fee689109f2998600d0fbee4
Michał Gibas <259563@student.pwr.edu.pl>
2022-06-13 17:10:59 +0200

    Add sample absolute error tests in main

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,55 @@
 #include "fp16.hpp"
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
 
-int main(void) {
-    std::cout << LDIV(FP16(1.0f), FP16(3.0f)) << '\n';
+void runTests(size_t repetitions);
+
+int main(void) { 
+    runTests(100);
+}
+
+void runTests(size_t repetitions) {
+    srand(time(NULL));
+
+    const size_t SCALAR_COUNT = 3;
+    const unsigned scalars[] = { 10, 100, 1000 };
+
+    std::cout << "LDIV: \n";
+    std::cout << "scalar, absError\n";
+    for(size_t i = 0; i < SCALAR_COUNT; ++i) {
+        float absError = 0.0f;
+        for(size_t j = 0; j < repetitions; ++j) {
+            float a = (float) (rand()%scalars[i]) + 1.0f;
+            float b = (float) (rand()%scalars[i]) + 1.0f;
+            absError += std::abs(LDIV(FP16(a), FP16(b)) - (a/b));
+        }
+
+        std::cout << scalars[i] << ", " << absError / (float)(repetitions) << '\n';
+    }
+
+    std::cout << "LSR: \n";
+    std::cout << "scalar, absError\n";
+    for(size_t i = 0; i < SCALAR_COUNT; ++i) {
+        float absError = 0.0f;
+        for(size_t j = 0; j < repetitions; ++j) {
+            float a = (float) (rand()%scalars[i]) + 1.0f;
+            absError += std::abs(LSR(FP16(a)) - sqrt(a));
+        }
+
+        std::cout << scalars[i] << ", " << absError / (float)(repetitions) << '\n';
+    }
+
+    std::cout << "LISR: \n";
+    std::cout << "scalar, absError\n";
+    for(size_t i = 0; i < SCALAR_COUNT; ++i) {
+        float absError = 0.0f;
+        for(size_t j = 0; j < repetitions; ++j) {
+            float a = (float) (rand()%scalars[i]) + 1.0f;
+            absError += std::abs(LISR(FP16(a)) - (1.0f/sqrt(a)));
+        }
+
+        std::cout << scalars[i] << ", " << absError / (float)(repetitions) << '\n';
+    }
+
 }
